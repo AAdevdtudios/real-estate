2ecededdc840ade9c2bf6dbfa8c2b330ca434f87
AAdevStudios <86613826+AAdevdtudios@users.noreply.github.com>
2023-04-01 15:31:55 +0100

    kk

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 
 export default defineNuxtConfig({
   css: ["@/assets/styles/app.css"],
-  modules: ["@nuxtjs/supabase"],
+  // modules: ["@nuxtjs/supabase"],
   build: {
     postcss: {
       postcssOptions: require("./postcss.config.js"),
